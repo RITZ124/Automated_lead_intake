c9da70d16bad8dceaea0e31dff3a9cba0afe2f1b
Rhitik Prajapati <140731787+RITZ124@users.noreply.github.com>
2026-05-19 12:35:37 +0530

    Update drive_service.py

--- app/services/drive_service.py
+++ app/services/drive_service.py
@@ -43,7 +43,7 @@ def authenticate_drive():
 
             flow = InstalledAppFlow.from_client_secrets_file(
 
-                "credentials/oauth_credentials.json",
+                "credentials/google_credentials.json",
 
                 SCOPES
 
